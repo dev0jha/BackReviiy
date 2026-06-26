ba003f8ff84576d3ab7028453755af7b6809919a
dev0jha <ojha6773@gmail.com>
2026-06-27 01:43:40 +0530

    refactor: add auth routes, body parsing middleware, and clean up Prisma schema

--- index.ts
+++ index.ts
@@ -1,15 +1,17 @@
 import type { Request, Response } from "express";
 import express from "express";
 import movieRouter from "./src/routes/movieRoute.ts";
+import authRoutes from "./src/routes/authRoutes.ts";
 
 const app = express();
 const PORT = 8000;
 
-app.use("/movies", movieRouter);
+//  Body Parsing Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Namskar Dev");
-});
+app.use("/movies", movieRouter);
+app.use("/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port :${PORT}`);
